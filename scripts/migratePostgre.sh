533b57ceea7f022172bda8733eed7b75f215f07e
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2023-02-13 09:29:54 +0100

    Add node_module/.bin to zip v6

--- scripts/migratePostgre.sh
+++ scripts/migratePostgre.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+basedir=$(dirname "$(echo "$0" | sed -e 's,\\,/,g')")
+echo "OVO je BASEDIR: ${basedir}"
+
+if [ -x "$basedir/node" ]; then
+  echo "THEN DEO - VUJO"
+else 
+  echo "ELSE DEO - VUJO"
+fi
+
 npx prisma migrate deploy
+# node node_modules\\prisma\\build\\index.js migrate deploy
