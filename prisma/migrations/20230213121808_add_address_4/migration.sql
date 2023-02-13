a5711e044516bde3f35781360ff401289f32acd6
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2023-02-13 13:18:53 +0100

    Execute migration script only once and add Address 4

---
+++ prisma/migrations/20230213121808_add_address_4/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Address_4" (
+    "id" SERIAL NOT NULL,
+    "street" TEXT NOT NULL,
+    "addressNumber" INTEGER NOT NULL,
+
+    CONSTRAINT "Address_4_pkey" PRIMARY KEY ("id")
+);
