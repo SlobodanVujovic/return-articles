75b6256b740722556841bfbce28554566596bee2
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2023-02-13 12:57:32 +0100

    Add Address 3 model

---
+++ prisma/migrations/20230213115654_add_address_3/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Address_3" (
+    "id" SERIAL NOT NULL,
+    "street" TEXT NOT NULL,
+    "addressNumber" INTEGER NOT NULL,
+
+    CONSTRAINT "Address_3_pkey" PRIMARY KEY ("id")
+);
